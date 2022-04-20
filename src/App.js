c274bdabc09c13ef6b178a9ba9a4b187daf1898d
jahangiralam7864 <wedevs95@gmail.com>
2022-04-21 01:10:46 +0600

    submit-code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // function App() {
 //   return (
@@ -52,27 +51,94 @@ import { useState } from 'react';
 // }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Counter></Counter>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Counter></Counter>
+//     </div>
+//   );
+// }
 
-function Counter() {
-  const [count, setCount] = useState(50);
+// function Counter() {
+//   const [count, setCount] = useState(50);
 
-  const increaseCount = () =>{
-  const newCount = count + 1;
-  setCount(newCount);
-  }
-  return (
+//   const increaseCount = () =>{
+//   const newCount = count + 1;
+//   setCount(newCount);
+//   }
+//   return (
+//     <div>
+//       <h1>Count : {count}</h1>
+//       <button onClick= {increaseCount}>Increase</button>
+//     </div>
+//   );
+// }
+
+
+ function App() {
+   return(
+     <div className='App'>
+       {/* <Counter></Counter> */}
+       <ExternalUsers></ExternalUsers>
+       <User></User>
+     </div>
+   );
+ }
+
+//  new component
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, []);
+
+  return(
     <div>
-      <h1>Count : {count}</h1>
-      <button onClick={increaseCount}>Increase</button>
+      <h1>External Users</h1>
+      {
+        users.map(user => <User 
+          id={user.id}
+          name={user.name} 
+          username={user.username}
+          email={user.email}
+          phone={user.phone}
+          ></User>)
+      }
     </div>
   );
 }
+
+function User(props) {
+  return(
+    <div className='styling'>
+      <p>ID: {props.id} </p>
+      <h2>Name: {props.name} </h2>
+      <h3>User Name: {props.username} </h3>
+      <p>Email: {props.email} </p>
+      <p>Phone: {props.phone} </p>
+    </div>
+  );
+}
+
+
+//  function Counter() {
+//    const [Count, setCount] = useState(10);
+//    const increaseCount = () =>setCount(Count + 1);
+//    const Decrease = () =>setCount(Count - 1);
+   
+//   //  console.log(jahangir);
+//    return(
+//      <div>
+//        <h1>Count: {Count} </h1>
+//        <button onClick={increaseCount}>Increase</button>
+//        <button onClick={Decrease}>Decrease</button>
+//      </div> 
+//    );
+//  }
 
 export default App;
+
+
+ 
